03d89b9063e4a2a7297b5c2d736ef2e63c45b2d1
eti1337 <etienne.baritaud.figueroa@outlook.com>
2016-12-16 23:16:08 +0100

    Remove useless stuff

--- .scripts/neofetch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-clear ; neofetch --image --ascii_colors 1 --disable kernel gpu packages font icons theme uptime memory resolution
--- .scripts/weather.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-clear
-
-weather -m -q LFBD | grep --color=always -e Temperature -e Wind -e Relative
-
