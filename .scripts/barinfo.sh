696cfbb13ff1af7ad7ad7e27de93053e7577db2b
eti1337 <etienne.baritaud.figueroa@outlook.com>
2017-06-01 23:37:23 +0200

    updated dots to the latest rice

--- .scripts/barinfo.sh
+++ .scripts/barinfo.sh
@@ -1,51 +1,68 @@
 #!/bin/bash
 
 clock() {
-	DATETIME=$(date +%R)
-	echo -n %{F#DFC39B}%{F#eee} $DATETIME
+	time=$(date +%R)
+	echo -n %{F#808080}%{F#eee} $time
 }
 
 sound() {
-	SOUND=$(amixer get Master 2>&1 | awk '/Front Left:/{gsub(/[\[\]]/, "", $5); print $5}')
-	echo %{F#DFC39B}%{F#eee} $SOUND
+	sound=$(amixer get Master 2>&1 | awk '/Front Left:/{gsub(/[\[\]]/, "", $5); print $5}')
+	echo %{F#808080}%{F#eee} $sound
 }
 
 song() {
-	CURRENTSONG=$(mpc -p 7755 current)
-	PLAYING=$(mpc -p 7755 status | grep -o 'playing')
+	current=$(mpc -p 7755 current)
+	playing=$(mpc -p 7755 status | grep -o 'playing')
 
-	if test "$PLAYING" = "playing"; then
-		echo %{F#DFC39B}%{F#eee} $CURRENTSONG
-	else test "$PLAYING" = "";
-		echo ''
+	if test "$playing" = "playing"; then
+		echo %{F#808080}%{F#eee} $current
+	else test "$playing" = "";
+		echo ""
 	fi
 }
 
+ping() {
+   pong=$(ping eti.tf | cut -d = -f 4-)
+   echo $pong
+}
+
 desktops() {
-	CDESKTOP=$(bash ~/.scripts/current-desktop.sh)
-	echo $CDESKTOP
+	cur=`xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}'`
+	tot=`xprop -root _NET_NUMBER_OF_DESKTOPS | awk '{print $3}'`
+
+	for w in `seq 0 $((cur - 1))`; do cdesktop="${cdesktop} ◽"; done
+	cdesktop="${cdesktop} %{F#eee}◾%{F#808080}"
+	for w in `seq $((cur + 2)) $tot`; do cdesktop="${cdesktop} ◽"; done
+
+	echo $cdesktop
 }
 
 network() {
-	CNETWORK=$(iwgetid -r)
-	echo %{F#DFC39B} %{F#eee} $CNETWORK
+	cnetwork=$(iwgetid -r)
+
+	if test "$cnetwork" = ""; then
+		echo ""
+	else
+		echo %{F#808080}%{F#eee} $cnetwork
+	fi
 }
 
 battery() {
     percent=$(cat /sys/class/power_supply/BAT0/capacity)
     power_now=$(cat /sys/class/power_supply/BAT0/power_now)
+		status=$(cat /sys/class/power_supply/BAT0/status)
 
     fullthing="${symbol}%{F#eee} ${percent}%"
 
-    if test $power_now = "0"; then
+    if test $status = "Charging"; then
         symbol=""
         echo -n "${symbol}%{F#eee} ${percent}%"
-    elif test $percent = "98"; then
+    elif test $percent = "99"; then
         symbol=""
         echo -n "${symbol}%{F#eee} 100%"
     else
         if test $percent -gt 60; then
-            symbol=""
+            symbol=""
             echo -n "${symbol}%{F#eee} ${percent}%"
         elif test $percent -gt 30; then
             symbol=""
@@ -59,6 +76,6 @@ battery() {
 
 # print all
 while true; do
-	echo "%{F#DFC39B} %{l} $(desktops)  $(song) %{c} $(clock) %{r} $(network)  $(sound) %{F#DFC39B} $(battery) "
-	sleep 0.1
+	echo "%{F#808080} %{l} $(desktops)  $(song) %{c} $(clock) %{r} $(network)  $(sound) %{F#808080} $(battery) "
+	sleep 0.4
 done
